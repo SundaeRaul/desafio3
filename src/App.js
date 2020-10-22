c6eb64ee8d64ce8442c6d86e58a2a3b03fd79f09
Fabio Frasson <fabio.frass@gmail.com>
2020-10-22 15:58:30 -0300

    Adições para organização

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fabio from './components/Fabio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default (props) => (
+  <div className="App">
+    <div className="container">
+      <Fabio>
+        <header className="header">
+          <h2>Lista de integrantes do grupo</h2>
+        </header>
+        <div className="integrantes">
+          <ul> 
+            <li>Fabio</li>
+            <li>Fernando</li>
+            <li>Luisa</li>
+            <li>Raul</li>
+          </ul>
+        </div>      
+      </Fabio>
+    </div>    
+  </div>
+)
