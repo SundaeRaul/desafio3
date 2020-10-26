2654c3afd218ed6997e9b2e6e16d03f34b82dcf9
Fabio Frasson <fabio.frass@gmail.com>
2020-10-26 19:53:59 -0300

    Ajuste componente Fabio

--- src/App.js
+++ src/App.js
@@ -3,25 +3,11 @@ import './App.css';
 import Fabio from './components/Fabio'
 import Form from './components/Form'
 
-export default (props) => (
+export default () =>
   <div className="App">
-    <div className="container">
-      <Fabio>
-        <header className="header">
-          <h2>Lista de integrantes do grupo</h2>
-        </header>
-        <div className="integrantes">
-          <ul> 
-            <li>Fabio</li>
-            <li>Fernando</li>
-            <li>Luisa</li>
-            <li>Raul</li>
-          </ul>
-        </div>      
+      <Fabio>       
       </Fabio>
 
       <Form/>
 
-    </div>    
-  </div>
-)
+    </div> 
--- src/components/Fabio.jsx
+++ src/components/Fabio.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../components/Fabio.css'
 
-export default props =>
-    <div>
-        <h1>Olá mundo ReactJS!</h1>
-        <div>
-            {props.children}
-        </div>
+export default () =>
+    <div className="container">
+        <h3 className="titulo">Componentes do grupo</h3>
+        <h2 className="subtitulo">Desafio 3</h2>
+        <ul className="list">
+            <li className="list-item"><a target="_blank" href="https://github.com/fabiofrasson">Fabio Frasson</a></li>
+            <li className="list-item"><a target="_blank" href="https://github.com/Fernando-Fritzen">Fernando Fritzen</a></li>
+            <li className="list-item"><a target="_blank" href="https://github.com/ghellereluisa">Luisa Ghellere</a></li>
+            <li className="list-item"><a target="_blank" href="https://github.com/SundaeRaul">Raul Rios</a></li>
+        </ul>
     </div>
