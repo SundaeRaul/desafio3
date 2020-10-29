990581ec3fbdfbf073cef58a080caa34c4ab0e0b
Sundaeraul <raulrios0907@outlook.com>
2020-10-28 22:02:11 -0300

    tela de login quase pronta

---
+++ src/components/Login-Components/Login-Verde.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login-Verde.css';
+
+function DivVerde(){
+    return (
+        <div className="verde">
+            <h2>Bem-vindo (a), Estudante!</h2>
+               <p>Insira seus dados e começe a controlar suas finanças!</p>
+                <input type="button" id="Cadastro" value="Cadastre-se!"/>            
+         </div> 
+    );
+}
+
+export default DivVerde;
--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import App from './pages/App';
 import Contato from './pages/Contato';
+import Login from './pages/Login';
 
 
 import { Switch, Route } from 'react-router-dom';
 
 
+
 function Routes(){
     return(
         <Switch>
-            <Route path="/" component={App} exact />
+            <Route path="/" component={App} exact/>
             <Route path="/Contato" component={Contato} />
+            <Route path="/Login" component={Login} />
         </Switch>
     );
 }
