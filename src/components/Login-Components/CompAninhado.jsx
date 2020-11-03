16ea7ffb46bfd00558ff35ac6693d988e1883670
Sundaeraul <raulrios0907@outlook.com>
2020-11-03 18:19:18 -0300

    Comp Aninhado e JSON

---
+++ src/components/Login-Components/CompAninhado.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login-Verde.css';
+
+function Text () {
+    return (
+        <p>Insira seus dados e começe a controlar suas finanças!</p>
+    );
+}
+
+export default Text;
--- src/components/Login-Components/Login-Verde.jsx
+++ src/components/Login-Components/Login-Verde.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Login-Verde.css';
+import Text from "./CompAninhado";
 
 function DivVerde(){
     return (
         <div className="verde">
             <h2>Bem-vindo (a), Estudante!</h2>
-               <p>Insira seus dados e começe a controlar suas finanças!</p>
+                <Text/>
                 <input type="button" className="Cadastro" value="Cadastre-se!" />            
          </div> 
     );
