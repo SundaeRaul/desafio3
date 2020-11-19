2903f281eca8efaf97366509187cb161e7dee9f7
ghellereluisa <luisaghellereramos@gmail.com>
2020-11-19 17:07:16 -0300

    card pag balanço

--- src/components/Inicial-Components/BalaçoGeral.jsx
+++ src/components/Inicial-Components/BalaçoGeral.jsx
@@ -4,7 +4,9 @@ import "./BalançoGeral.css";
 function Balanço(){
     return(
         <div className="CardVerde">
-            
+            <h2 className="NomePessoa"> Olá, (nome) </h2>
+            <h1 className="Saudo"> R$ (valor) </h1>
+            <h3 className="Balanço"> Balanço geral </h3>
         </div>
     )
 
