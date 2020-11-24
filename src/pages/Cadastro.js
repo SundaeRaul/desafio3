cf295df94f7b61da9a228bf35002f22cd2315f95
ghellereluisa <luisaghellereramos@gmail.com>
2020-11-24 19:51:33 -0300

    css

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -2,7 +2,7 @@ import React from  'react'
 import '../styles/Cadastro.css'
 import DivVerdeCadastro from '../components/Cadastro-Components/Verde-components'
 import DivBrancaCadastro from '../components/Cadastro-Components/Branco-components'
-import MainLogo from '../components/Login-components/MainLogo'
+import MainLogo from '../components/Login-Components/MainLogo'
 
 
 function Cadastro () {
