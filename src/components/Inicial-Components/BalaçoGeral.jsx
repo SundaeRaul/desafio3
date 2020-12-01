8f845c3c6fc099a3e65c8644deb2f9a7afc89a3d
ghellereluisa <luisaghellereramos@gmail.com>
2020-11-30 21:44:59 -0300

    inicial redone

--- src/components/Inicial-Components/BalaçoGeral.jsx
+++ src/components/Inicial-Components/BalaçoGeral.jsx
@@ -3,8 +3,8 @@ import "./BalançoGeral.css";
 
 function Balanço(){
     return(
-        <div className="CardVerde">
-            <h1 className="NomePessoa"> Olá, Luisa Ghellere </h1>
+        <div className="boxVerde">
+            <h1 className="NomePessoa"> Olá, Luisa Ghellere! </h1>
             <h1 className="Saudo"> R$ 850,00 </h1>
             <h2 className="Balanço"> Balanço geral </h2>
         </div>
