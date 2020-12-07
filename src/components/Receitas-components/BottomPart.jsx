8540317b16f0f48267c52705b4a67870ec881ecb
Fabio Frasson <fabio.frass@gmail.com>
2020-12-07 13:36:30 -0300

    inserção parcial código hook useState

--- src/components/Receitas-components/BottomPart.jsx
+++ src/components/Receitas-components/BottomPart.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './BottomPart.css';
-import ListaDados from '../Receitas-components/ListaDados';
+import React from "react";
+import "./BottomPart.css";
+import ListaDados from "../Receitas-components/ListaDados";
 
 class BottomPart extends React.Component {
-    render() {
-        return(
-            <div className="btm-part">
-                <ListaDados />
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className="btm-part">
+        <ListaDados />
+      </div>
+    );
+  }
 }
 
 export default BottomPart;
