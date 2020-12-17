121debe4ffecc26cafa62b6a44f7a068afcf81a3
SundaeRaul <raulrios0907@outlook.com>
2020-12-17 14:59:30 -0300

    logo nas paginas

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../styles/Cadastro.css';
 import BoxCadastro from '../components/Cadastro-Components/BoxCadastro';
+import Logo from '../images/logo2.png';
+import {Link} from 'react-router-dom';
 
 
 function Cadastro(){
     return(
         <div id="Cadastro">
+            <Link to="/" title="FinanC" style={{textDecoration: 'none', color:'none'}}>
+                <img className="LogoLogin" src={Logo}/>
+            </Link>
             <BoxCadastro />
         </div>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Login.css';
 import BoxLogin from '../components/Login-Components/BoxLogin'
+import Logo from "../images/logo2.png"
+import { Link } from 'react-router-dom';
 
 function Login(){
     return(
         <div id="Login">
+            <Link to="/" title="FinanC" style={{textDecoration: 'none', color:'none'}}>
+                <img className="LogoLogin" src={Logo}/>
+            </Link>
             <BoxLogin />
         </div>
     );
